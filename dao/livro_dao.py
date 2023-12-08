503805423b24250995adb1e2f4c4150e93ef5dfb
Andre <104678440+andrefrencl@users.noreply.github.com>
2023-12-08 00:15:56 +0000

    dao finalizado com psql

--- dao/livro_dao.py
+++ dao/livro_dao.py
@@ -1,39 +1,94 @@
 from model.livro import Livro
+from database.conexao_factory import ConexaoFactory
+from dao.categoria_dao import CategoriaDAO
+from dao.editora_dao import EditoraDAO
+from dao.autor_dao import AutorDAO
+
 
 class LivroDAO:
 
-    def __init__(self):
+    def __init__(self, categoria_dao: CategoriaDAO, editora_dao: EditoraDAO, autor_dao: AutorDAO):
         self.__livros: list[Livro] = list()
+        self.__conexao_factory: ConexaoFactory = ConexaoFactory()
+        self.__categoria_dao: CategoriaDAO = categoria_dao
+        self.__editora_dao: EditoraDAO = editora_dao
+        self.__autor_dao = AutorDAO = autor_dao
 
     def listar(self) -> list[Livro]:
-        return self.__livros
+        livros = list()
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute(
+            'select id, titulo, resumo, ano, paginas, isbn, categoria_id, autor_id, editora_id from livros')
+        resultados = cursor.fetchall()
+        for resultado in resultados:
+            # por categoria ser um objeto, precisa tratar cada id para exibir a ref de cada id
+            categoria = self.__categoria_dao.buscar_por_id(resultado[6])
+            editora = self.__autor_dao.buscar_por_id(resultado[7])
+            autor = self.__editora_dao.buscar_por_id(resultado[8])
+
+            liv = Livro(resultado[1], resultado[2], int(resultado[3]), int(resultado[4]),
+                        resultado[5], categoria, editora, autor)
+
+            liv.id = resultado[0]
+            livros.append(liv)
+        cursor.close()
+        conexao.close()
+        return livros
 
     def adicionar(self, livro: Livro) -> None:
-        self.__livros.append(livro)
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            insert into livros (titulo, resumo, ano, paginas, isbn, categoria_id, autor_id, editora_id) 
+                values (%(titulo)s, %(resumo)s, %(ano)s, %(paginas)s, %(isbn)s, 
+                        %(categoria_id)s, %(autor_id)s, %(editora_id)s
+                        )
+            ''',
+                       ({'titulo': livro.titulo, 'resumo': livro.resumo, 'ano': livro.ano,
+                         'paginas': livro.paginas, 'isbn': livro.isbn,
+                         'categoria_id': livro.categoria.id, 'autor_id': livro.autor.id,
+                         'editora_id': livro.editora.id})
+                       )
+
+        conexao.commit()
+        cursor.close()
+        conexao.close()
 
     def remover(self, livro_id: int) -> bool:
-        encontrado = False
-        for l in self.__livros:
-            if (l.id == livro_id):
-                index = self.__livros.index(l)
-                self.__livros.pop(index)
-                encontrado = True
-                break
-        return encontrado
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute('delete from livros where id=%s', (livro_id,))
+        removidas = cursor.rowcount
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+        if removidas == 0:
+            return False
+        else:
+            return True
 
     def buscar_por_id(self, livro_id) -> Livro:
         liv = None
-        for l in self.__livros:
-            if (l.id == livro_id):
-                liv = l
-                break
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute(
+            "select id, titulo, resumo, ano, paginas, isbn, categoria_id, autor_id, editora_id "
+            "from livros "
+            "where id = %s", (livro_id,)
+        )
+        resultado = cursor.fetchone()  # se vier só um resultado, é fetchone
+        # fechall se vier uma lista
+        if resultado:
+            categoria = self.__categoria_dao.buscar_por_id(resultado[6])
+            editora = self.__autor_dao.buscar_por_id(resultado[7])
+            autor = self.__editora_dao.buscar_por_id(resultado[8])
+
+            liv = Livro(resultado[1], resultado[2], int(resultado[3]), int(resultado[4]),
+                        resultado[5], categoria, editora, autor)
+
+            liv.id = resultado[0]
+
+        cursor.close()
+        conexao.close()
         return liv
-    
-    def ultimo_id(self) -> int:
-        index = len(self.__livros) -1
-        if (index == -1):
-            id = 0
-        else:
-            id = self.__livros[index].id
-        return id
-    
